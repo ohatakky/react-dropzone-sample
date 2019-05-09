7b767d6296efa252408bbc56db0b45eed1b433b1
ohatakky <ohatakky@gmail.com>
2019-05-09 10:57:24 +0900

    use wrapper component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import MyDropzone from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<MyDropzone />, document.getElementById('root'));
+import WrapHooks from './WrapHooks';
+
+ReactDOM.render(
+  <React.Fragment>
+    <WrapHooks />
+    {/* <MyDropzone /> */}
+  </React.Fragment>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
